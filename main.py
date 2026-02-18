d6bb6a7979fd21bf1ca6722776bb4a1011155ae6
theflipper <flipya1423@gmail.com>
2026-02-18 19:27:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -34,10 +34,7 @@ logger.addHandler(file_handler)
 # --- НАСТРОЙКИ ---
 SOURCES = [
     # Твои оригинальные источники
-    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-all.txt",
-    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile.txt",
-    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile-2.txt",
-    "https://clck.ru/3RcLDw",
+    "https://clck.ru/3RcLDw"
 
 ]
 
