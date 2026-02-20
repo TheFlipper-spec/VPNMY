b3bbdb06fc1475a2ca532b0cbd9df898836fb9b6
theflipper <flipya1423@gmail.com>
2026-02-20 18:36:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ SOURCES = [
      "https://raw.githubusercontent.com/TheFlipper-spec/VPNMY/refs/heads/main/my_source",
      "https://raw.githubusercontent.com/Rayan-Config/C-Sub/refs/heads/main/configs/proxy.txt",
 
+"https://raw.githubusercontent.com/hamedcode/port-based-v2ray-configs/main/sub/vless.txt" 
 
 ]
 
