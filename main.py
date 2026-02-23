77e1f3550de4cd84839e8a4db1fdcb8518a06683
theflipper <flipya1423@gmail.com>
2026-02-23 04:26:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,8 +37,7 @@ SOURCES = [
 "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/BLACK_VLESS_RUS_mobile.txt#BLACK_VLESS_RUS_mobile",
 
 #Тест
-"https://raw.githubusercontent.com/FLEXIY0/matryoshka-vpn/refs/heads/main/configs/russia_whitelist.txt", 
-"https://raw.githubusercontent.com/Argh94/V2RayAutoConfig/refs/heads/main/configs/Estonia.txt" 
+"https://raw.githubusercontent.com/FLEXIY0/matryoshka-vpn/refs/heads/main/configs/russia_whitelist.txt"
 ]
 
 XRAY_BIN = "./xray"
