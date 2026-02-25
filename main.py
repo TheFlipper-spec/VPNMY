c9e6711ed91b4114242eae64fe74b2a0ff408656
theflipper <flipya1423@gmail.com>
2026-02-25 16:11:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,8 @@ SOURCES = [
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-all.txt",
     "https://clck.ru/3RcLDw",
 "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/BLACK_VLESS_RUS_mobile.txt#BLACK_VLESS_RUS_mobile",
-"https://raw.githubusercontent.com/Argh94/V2RayAutoConfig/refs/heads/main/configs/Latvia.txt" 
+"https://raw.githubusercontent.com/Argh94/V2RayAutoConfig/refs/heads/main/configs/Latvia.txt",
+"https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-checked.txt"  
 
 ]
 
