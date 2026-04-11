06a89fed11a0f2cfa9f55bb4a348264b5409cb8a
theflipper <flipya1423@gmail.com>
2026-04-11 16:45:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ SOURCES = [
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/BLACK_VLESS_RUS_mobile.txt",
     "https://gbr.mydan.online/configs",
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-checked.txt",
-    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-SNI-RU-all.txt" 
+    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-SNI-RU-all.txt",
+"https://gistpad.com/raw/mia-vpn-tg-reverse-engineer-s-basement#mia-vpn-tg-reverse-engineer-s-basement" 
 ]
 
 XRAY_BIN = "./xray"
